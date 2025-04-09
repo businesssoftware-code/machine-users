f5d51bdbbe2178300281b72e19faedd6ea7a8124
Shubhcyb2611 <s.gupta@cybtekk.com>
2025-04-09 18:03:26 +0530

    feat/added user service

--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -1,13 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from '../../prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '../../src/common/auth/jwt.service';
 
 @Injectable()
 export class UserService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async getUserById(id: number) {
     return await this.prisma.user.findUnique({
       where: { id: id },
     });
+  }
+
+  async userLogin(id: number, password: string) {
+    const user = await this.validateUser(id, password);
+    const accessToken = await this.jwtService.createAccessToken(
+      user.id,
+      user.name,
+    );
+    const refreshToken = await this.jwtService.createRefreshToken(
+      user.id,
+      user.name,
+    );
+
+    return {
+      user,
+      accessToken: accessToken,
+      refreshToken: refreshToken,
+    };
+  }
+
+  async validateUser(id: number, password: string) {
+    const user = await this.prisma.user.findUnique({ where: { id: id } });
+    if (!user) {
+      throw new UnauthorizedException('User not found');
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    return user;
   }
 }
