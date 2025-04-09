d694764c517003c6499d2c74afa1deb91fa9aa76
Shubhcyb2611 <s.gupta@cybtekk.com>
2025-04-09 15:24:40 +0530

    feat/added cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { corsOptions } from './common/cors.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors(corsOptions);
+  await app.listen(3090);
 }
 bootstrap();
